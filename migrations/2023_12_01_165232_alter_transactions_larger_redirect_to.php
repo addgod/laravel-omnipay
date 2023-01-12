d0cd451eee1c0abe4ebf549cbed352e710bfc162
Jesper Kold-Hansen <jkoldhansen@gmail.com>
2023-01-12 15:49:21 +0100

    Added migratsion for lager redirect column

---
+++ migrations/2023_12_01_165232_alter_transactions_larger_redirect_to.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTransactionsLargerRedictTo extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('omnipay_transactions', function (Blueprint $table) {
+            $table->text('redirect_to')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // One way transaction.
+    }
+}
